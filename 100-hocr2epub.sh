5b01186be503fc9faf48214130c65abf486ea55c
Milan Hauth <milahu@gmail.com>
2026-02-02 21:34:44 +0000

    up 100-hocr2epub.sh

--- 100-hocr2epub.sh
+++ 100-hocr2epub.sh
@@ -34,7 +34,33 @@ args+=(
   --scale "$scale"
   --image-format avif
   --text-format html
-  --doc-title "$doc_title"
+  # --doc-title "$doc_title"
+  --doc-title "Tödliches Vermächtnis"
+  --doc-subtitle "Wie das Unkrautvernichtungsmittel Glyphosat unsere Gesundheit und Umwelt zerstört"
+  --doc-description "Kein anderes Herbizid hat die landwirtschaftliche Massenproduktion so beeinflusst wie Glyphosat.
+Dabei wurden schädliche Auswirkungen auf Mensch und Natur jahrzehntelang ignoriert, verleugnet, vertuscht,
+kritische Wissenschaftler und Journalisten zum Schweigen gebracht.
+Kein Wunder, wird der weltweite Markt doch auf ein Volumen von etwa zehn Milliarden US-Dollar geschätzt
+und werden jährlich mindestens eine Million Tonnen des Unkrautvernichters versprüht.
+
+Nun liefert die ehemalige leitende Wissenschaftlerin des Massachusetts Institute of Technology (MIT) Dr. Stephanie Seneff
+unumstößliche Beweise für die verheerenden gesundheitlichen und ökologischen Wirkungen der Substanz und belegt,
+dass Glyphosat als wesentliche Ursache für die Entstehung von Zivilisationskrankheiten
+wie Autismus, Parkinson, Alzheimer, Autoimmun-Erkrankungen und Krebs anzusehen ist,
+wobei sich die schädigende Wirkung im Zusammenspiel mit anderen Giftstoffen noch verstärkt."
+  --doc-subject "Kriminalität in Wirtschaft & Politik, Mafia & Organisiertes Verbrechen, Erziehung"
+  --doc-modified "$(git show -s --format=%cI HEAD)"
+  --doc-date 2025-10-20
+  --doc-edition 1
+  --doc-extent "372 pages"
+  --doc-author "Stephanie Seneff"
+  --doc-introducer "Jens Wernicke"
+  --doc-contributor "Wolfgang Wodarg"
+  --doc-translator "Hans-Michael Hackenberg"
+  --doc-publisher "Rubikon-Verlag"
+  --doc-language de
+  --doc-isbn 9783952595480
+  --doc-cover-image 070-deskew/373.tiff
   --canonical-url-base https://milahu.github.io/toedliches-vermaechtnis-glyphosat-stephanie-seneff-2025/
 )
 todo_args+=(
